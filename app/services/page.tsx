a6891ddc05e2515a9c43e07fcf442838bfa2e0ca
John Larson <jl@buildmidwestern.com>
2024-03-16 10:17:19 -0500

    fixed nav

---
+++ app/services/page.tsx
@@ -0,0 +1,13 @@
+import CTA from '../../components/CTA';
+import DemoCarousel from '../../components/DemoCarousel';
+import Hero from '../../components/Hero';
+
+export default function Services() {
+  return (
+    <main>
+      <Hero />
+      <DemoCarousel />
+      <CTA />
+    </main>
+  );
+}
